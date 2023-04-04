34a3908a1cd4b6d9634684d70b4221d30181486b
unknown <ihechukelechijp@gmail.com>
2023-04-04 17:10:19 +0100

    my section 8 assignment

---
+++ RANDOM/app/age.py
@@ -0,0 +1,15 @@
+import random
+
+#age = int(input())
+
+#if age >= 18:
+    
+   # print("True")
+#else:
+    #print("False")
+
+age = int(input("Enter your age"))
+if age >= 18: 
+    print("True")
+else:
+    print("False")
---
+++ RANDOM/training_code/boolapp.py
@@ -0,0 +1,29 @@
+import random
+#a = 200
+#b = 33
+
+#if b > a:
+ # print("b is greater than a")
+#else:
+ # print("b is not greater than a")
+
+
+#x = int(input("Enter a number"))
+#y = int(input ("Enter another number: "))
+#print(x)
+#if x < y or y >x:
+ #   print("True")
+#else:
+#    print("False")
+
+def myFunction() :
+ return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+
+
+
